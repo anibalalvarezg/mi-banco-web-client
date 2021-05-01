c85304fdad49fe24c61f05ae09dfbda20a23eca3
Aníbal Álvarez <anibal.alvarezg@gmail.com>
2021-05-01 16:14:45 -0400

    [Auth] Bootstrap installation and  login/register routes

---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthComponent } from './components/auth/auth.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    AuthComponent,
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
